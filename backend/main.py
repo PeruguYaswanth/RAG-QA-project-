1665030042dc64f147cd7779bc922e65fd007848
perugu Yaswanth <peruguyaswanth910@gmail.com>
2026-08-12 22:47:38 +0530

    fixed cros

--- backend/main.py
+++ backend/main.py
@@ -50,7 +50,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://rag-qa-project-production-2063.up.railway.app",
+        "https://rag-qa-project-o5boi4rs8-peruguyaswanths-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
