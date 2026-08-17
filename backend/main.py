00b29a5ef8033f4d044872ea8661238ec231b17c
perugu Yaswanth <peruguyaswanth910@gmail.com>
2026-08-17 23:40:49 +0530

    fixed cors issue

--- backend/main.py
+++ backend/main.py
@@ -64,6 +64,7 @@ app = FastAPI(title="RAG PDF QA Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://rag-qa-project-pmfc.vercel.app/"
         "https://rag-qa-project.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
